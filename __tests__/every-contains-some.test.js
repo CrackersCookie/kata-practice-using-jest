7f0c8d4532388565cef2df28e9ca0af43a06cc62
Mark Cooke <crackerscookie@yahoo.com>
2019-09-04 12:08:57 +0100

    add set up for every-some-contains

---
+++ __tests__/every-contains-some.test.js
@@ -0,0 +1,19 @@
+const { every, contains, some } = require('../katas/every-contains-some');
+
+describe('every', () => {
+  it('', () => {
+
+  });
+});
+
+describe('contains', () => {
+  it('', () => {
+
+  });
+});
+
+describe('some', () => {
+  it('', () => {
+
+  });
+});
---
+++ katas/every-contains-some.js
@@ -0,0 +1,17 @@
+const every = () => {
+
+}
+
+const contains = () => {
+
+}
+
+const some = () => {
+
+}
+
+module.exports = {
+  every,
+  contains,
+  some
+};
