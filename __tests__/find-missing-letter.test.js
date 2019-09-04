599973fc98cd903f52f2a4d0d6d490814b784f23
Mark Cooke <crackerscookie@yahoo.com>
2019-09-04 16:31:33 +0100

    solved missing-letter-kata

--- __tests__/find-missing-letter.test.js
+++ __tests__/find-missing-letter.test.js
@@ -1,7 +1,18 @@
 const { findMissingLetter } = require('../katas/find-missing-letter');
 
 describe('findMissingLetter', () => {
-  it('', () => {
-
-  });
+  it('returns an empty string when passed an empty array', () => {
+    expect(findMissingLetter([])).toBe('');
+  })
+  it('returns a string with the missing letter when passed an array of length 2 - which should be 3', () => {
+    expect(findMissingLetter(['A', 'C'])).toBe('B');
+  })
+  it('returns the correct letter when passed a longer uppercase string', () => {
+    const arr = ['A', 'B', 'C', 'E'];
+    expect(findMissingLetter(arr)).toBe('D');
+  })
+  it('returns the correct letter when passed a lower case string', () => {
+    const arr = ['e', 'f', 'g', 'i'];
+    expect(findMissingLetter(arr)).toBe('h');
+  })
 });
